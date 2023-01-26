4be624daf7fec47175961832a2d272307eb644a1
adamgonzo <86032875+adamgonzo@users.noreply.github.com>
2023-01-26 09:42:46 -0800

    updated the alt tags for seo

--- src/container/Swiper/Swiper.jsx
+++ src/container/Swiper/Swiper.jsx
@@ -1,16 +1,16 @@
 // import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
-import { images } from '../../constants';
+import { images } from "../../constants";
 
 // Import Swiper styles
-import 'swiper/scss';
-import 'swiper/scss/navigation';
-import 'swiper/scss/pagination';
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
 
-import './Swipe.scss';
+import "./Swipe.scss";
 const SwiperImage = () => {
   return (
     <Swiper
@@ -23,24 +23,90 @@ const SwiperImage = () => {
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
       onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
+      onSlideChange={() => console.log("slide change")}
     >
-      <SwiperSlide><img className="swipeImage" src={images.image1} alt="window"/></SwiperSlide>
-      <SwiperSlide><img className="swipeImage" src={images.image2} alt="window"/></SwiperSlide>
-      <SwiperSlide><img className="swipeImage" src={images.image3} alt="window"/></SwiperSlide>
-      <SwiperSlide><img className="swipeImage" src={images.image4} alt="window"/></SwiperSlide>
-      <SwiperSlide><img className="swipeImage" src={images.image5} alt="window"/></SwiperSlide>
-      <SwiperSlide><img className="swipeImage" src={images.image6} alt="window"/></SwiperSlide>
-      <SwiperSlide><img className="swipeImage" src={images.image7} alt="window"/></SwiperSlide>
-      <SwiperSlide><img className="swipeImage" src={images.image8} alt="window"/></SwiperSlide>
-      <SwiperSlide><img className="swipeImage" src={images.image10} alt="window"/></SwiperSlide>
-      <SwiperSlide><img className="swipeImage" src={images.image11} alt="window"/></SwiperSlide>
-      <SwiperSlide><img className="swipeImage" src={images.image12} alt="window"/></SwiperSlide>
-      <SwiperSlide><img className="swipeImage" src={images.image13} alt="window"/></SwiperSlide>
-      <SwiperSlide><img className="swipeImage" src={images.image14} alt="window"/></SwiperSlide>
-      <SwiperSlide><img className="swipeImage" src={images.image15} alt="window"/></SwiperSlide>
-
-
+      <SwiperSlide>
+        <img className="swipeImage" src={images.image1} alt="Glass Railing" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          className="swipeImage"
+          src={images.image2}
+          alt="Frameless Shower"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          className="swipeImage"
+          src={images.image3}
+          alt="Glass Stair Railing"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          className="swipeImage"
+          src={images.image4}
+          alt="Retroactive storefront"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          className="swipeImage"
+          src={images.image5}
+          alt="Store Front Windows"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          className="swipeImage"
+          src={images.image6}
+          alt="Frameless Shower"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="swipeImage" src={images.image7} alt="Stair Railing" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          className="swipeImage"
+          src={images.image8}
+          alt="Balcony Glass Railing"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          className="swipeImage"
+          src={images.image10}
+          alt="Sliding glass door"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="swipeImage" src={images.image11} alt="Glass Railing" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          className="swipeImage"
+          src={images.image12}
+          alt="Frameless Shower"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img className="swipeImage" src={images.image13} alt="Glass Railing" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          className="swipeImage"
+          src={images.image14}
+          alt="retroactive windows"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          className="swipeImage"
+          src={images.image15}
+          alt="Bay Area Glazer"
+        />
+      </SwiperSlide>
     </Swiper>
   );
 };
